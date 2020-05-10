904b6dd0d5593553bdef37350f987df571c212f8
Kent Martin Åsvang <kentaasvang@gmail.com>
2020-05-10 12:49:30 +0000

    oijo

--- main.go
+++ main.go
@@ -3,26 +3,25 @@ package main
 
 import (
     "net/http"
-    "html/template"
     "log"
+    "io/ioutil"
+    "html/template"
 )
 
 type Page struct {
 	Title string
 }
 
-
-func indexHandler(w http.ResponseWriter, r *http.Request)  {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	page := Page{"Posts"}
-	t, _ := template.ParseFiles("index.html")
-	t.Execute(w, page)
+	template, err := template.ParseFiles("~/go/src/github.com/kentaasvang/personal_website/index.html")
+	if (err != nil) {
+		ioutil.WriteFile("go-log.log", []byte(err.Error()), 0644)
+	}
+	template.Execute(w, page)
 }
-
 
 func main() {
     http.HandleFunc("/", indexHandler)
-    http.Handle("/vendor/", http.StripPrefix("/vendor/", http.FileServer(http.Dir("vendor"))))
-
-
     log.Fatal(http.ListenAndServe(":9990", nil))
 }
