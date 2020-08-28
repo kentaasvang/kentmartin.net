d7c85956f9a2d0c1dcd4ff5700c6283ec8a57e97
Kent Åsvang <kentaasvang@gmail.com>
2020-08-28 07:51:36 +0200

    added project path (hardcoded)

--- main.go
+++ main.go
@@ -1,47 +1,56 @@
 package main
 
-
 import (
-    "path"
-    "net/http"
-    "html/template"
-    "log"
-    "io/ioutil"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path"
 )
-
 
 type Page struct {
 	Title string
 }
 
+// TODO: this should _NOT_ be hardcoded
+// TODO: this should _NOT_ be hardcoded
+// TODO: this should _NOT_ be hardcoded
+var CurrentDirectory string =
+	"/Users/kent/go/src/github.com/kentaasvang/personal_website"
 
-var CurrentDirectory string = "/home/kent/go/src/github.com/kentaasvang/personal_website"
-
-
-func indexHandler(w http.ResponseWriter, r *http.Request)  {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	page := Page{"Posts"}
 	t, err := template.ParseFiles(path.Join(CurrentDirectory, "index.html"))
 
-	if (err != nil) {
+	if err != nil {
 		ioutil.WriteFile("go-log-file.log", []byte(err.Error()), 0644)
 	}
 
 	t.Execute(w, page)
 }
 
-
 func main() {
-    http.HandleFunc("/", indexHandler)
-    http.Handle(
-	"/vendor/",
-	http.StripPrefix("/vendor/", http.FileServer(http.Dir(path.Join(CurrentDirectory, "vendor")))))
 
-    http.Handle(
-	"/static/css/",
-	http.StripPrefix("/static/css/", http.FileServer(http.Dir(path.Join(CurrentDirectory, "static/css")))))
-    http.Handle(
-	"/static/js/",
-	http.StripPrefix("/static/js/", http.FileServer(http.Dir(path.Join(CurrentDirectory, "static/js")))))
+	// Routes
+	http.HandleFunc("/", indexHandler)
 
-    log.Fatal(http.ListenAndServe(":9990", nil))
+	// jQuery, bootstrap etc
+	http.Handle(
+		"/vendor/",
+		http.StripPrefix("/vendor/", http.FileServer(
+			http.Dir(path.Join(CurrentDirectory, "vendor")))))
+
+	// Custom CSS
+	http.Handle(
+		"/static/css/",
+		http.StripPrefix("/static/css/", http.FileServer(
+			http.Dir(path.Join(CurrentDirectory, "static/css")))))
+
+	// Custom JS
+	http.Handle(
+		"/static/js/",
+		http.StripPrefix("/static/js/", http.FileServer(
+			http.Dir(path.Join(CurrentDirectory, "static/js")))))
+
+	log.Fatal(http.ListenAndServe(":9990", nil))
 }
