2da84d917604372676e883a77b1b963a27fba27a
Kent Martin Åsvang <kent@kentmartin.net>
2020-08-28 08:47:01 +0000

    temp

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+	"github.com/joho/godotenv"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -8,19 +10,15 @@ import (
 	"path"
 )
 
+
 type Page struct {
 	Title string
 }
 
-// TODO: this should _NOT_ be hardcoded
-// TODO: this should _NOT_ be hardcoded
-// TODO: this should _NOT_ be hardcoded
-var CurrentDirectory string =
-	"/Users/kent/go/src/github.com/kentaasvang/personal_website"
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	page := Page{"Posts"}
-	t, err := template.ParseFiles(path.Join(CurrentDirectory, "index.html"))
+	page := Page{"Kentmartin | Hjem"}
+	t, err := template.ParseFiles(path.Join(os.Getenv("PROJECT_PATH"), "index.html"))
 
 	if err != nil {
 		ioutil.WriteFile("go-log-file.log", []byte(err.Error()), 0644)
@@ -30,6 +28,8 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil { log.Fatal("Error loading .env file") }
 
 	// Routes
 	http.HandleFunc("/", indexHandler)
@@ -38,19 +38,19 @@ func main() {
 	http.Handle(
 		"/vendor/",
 		http.StripPrefix("/vendor/", http.FileServer(
-			http.Dir(path.Join(CurrentDirectory, "vendor")))))
+			http.Dir(path.Join(os.Getenv("PROJECT_PATH"), "vendor")))))
 
 	// Custom CSS
 	http.Handle(
 		"/static/css/",
 		http.StripPrefix("/static/css/", http.FileServer(
-			http.Dir(path.Join(CurrentDirectory, "static/css")))))
+			http.Dir(path.Join(os.Getenv("PROJECT_PATH"), "static/css")))))
 
 	// Custom JS
 	http.Handle(
 		"/static/js/",
 		http.StripPrefix("/static/js/", http.FileServer(
-			http.Dir(path.Join(CurrentDirectory, "static/js")))))
+			http.Dir(path.Join(os.Getenv("PROJECT_PATH"), "static/js")))))
 
 	log.Fatal(http.ListenAndServe(":9990", nil))
 }
