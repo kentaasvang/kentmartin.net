4e23c09bf49be40bf8df173b8c314c7c026dbbbe
Kent Åsvang <kentaasvang@gmail.com>
2021-01-24 21:46:31 +0100

    a fresh start

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?= "<h1> kentmartin.dev </h1>"; ?>
+<h1> Hello welcome to blog </h1>
+
+
+<div>
+    <ul>
+        <li>
+            <h3> This is a Title </h3>
+            <p> this is a lot of information </p>
+        <li>
+            <h3> This is a Title </h3>
+            <p> this is a lot of information </p>
+        </li>
+    </ul>
+</div>
