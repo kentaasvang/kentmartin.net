be6b5431b8cd3b9d122611b67f944bf6440dd186
Kent Åsvang <kentaasvang@gmail.com>
2021-01-31 15:42:33 +0100

    starter-template kinda finished

---
+++ navigation.php
@@ -0,0 +1,32 @@
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Kentmartin.dev</a>
+    <button 
+      class="navbar-toggler" 
+      type="button" 
+      data-bs-toggle="collapse" 
+      data-bs-target="#navbarTogglerDemo02" 
+      aria-controls="navbarTogglerDemo02" 
+      aria-expanded="false" 
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="justify-content-end collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Hjem</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tjenester</a>
+        </li>
+        <li class="nav-item">
+          <a href="https://www.buymeacoffee.com/kentmartin"><img class="buy-me-a-coffee" src="https://img.buymeacoffee.com/button-api/?text=Spander en kaffe!&emoji=&slug=kentmartin&button_colour=40DCA5&font_colour=ffffff&font_family=Lato&outline_colour=000000&coffee_colour=FFDD00"></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
